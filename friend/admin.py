a43ab3eb1a0a19bdca511f6eb67a2c372247290c
brinkbrink <brinkbrink@gmail.com>
2022-03-20 11:12:19 -0700

    v5

--- friend/admin.py
+++ friend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Season, Foraged, Foray, Resource
 # Register your models here.
+admin.site.register(Season)
+admin.site.register(Foraged)
+admin.site.register(Foray)
+admin.site.register(Resource)
--- friend/models.py
+++ friend/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Season(models.Model):
+    season=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.season
+
+    class Meta:
+        db_table='seasons'
+
+class ForageType(models.Model):
+    typename=models.CharField(max_length=20)
+    typedescription=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.typename
+
+    class Meta:
+        db_table='foragetypes'
+
+class Foraged(models.Model):
+    name=models.CharField(max_length=250)
+    foragetype=models.ForeignKey(ForageType, on_delete=models.DO_NOTHING)
+    season=models.ForeignKey(Season, on_delete=models.DO_NOTHING)
+    datefound=models.DateField()
+    amountfound=models.FloatField()
+    location=models.CharField(max_length=250)
+    comment=models.TextField(null=True, blank=True)
+    dateentered=models.DateField()
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='foraged'
+
+class Foray(models.Model):
+    name=models.CharField(max_length=250)
+    location=models.CharField(max_length=250)
+    date=models.DateField()
+    time=models.TimeField()
+    description=models.TextField(null=True, blank=True)
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='forays'
+
+class Resource(models.Model):
+    name=models.CharField(max_length=250)
+    type=models.CharField(max_length=250)
+    url=models.URLField()
+    dateentered=models.DateField()
+    userid=models.ForeignKey(User, on_delete=models.CASCADE)
+    description=models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table='resources'
