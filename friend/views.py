1b090d8762f2e45d10091dd05640b4e06f2159d6
brinkbrink <brinkbrink@gmail.com>
2022-03-20 11:27:47 -0700

    v6

--- friend/views.py
+++ friend/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Foraged, ForageType, Season, Foray, Resource
 
 # Create your views here.
 def index(request):
     return render(request, 'friend/index.html')
+
+def foraged(request):
+    foraged_list=Foraged.objects.all()
+    return render(request, 'friend/foraged.html', {'foraged_list': foraged_list})
